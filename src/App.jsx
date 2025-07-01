b6fd3dcbf4e8892ebb4e420bde9ec12b1b3c7667
moeeztbox <moeez.tbox@gmail.com>
2025-07-01 22:02:17 +0500

    no change

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import FeedBackData from "./FeedBackData";
 import FeedBackList from "./FeedBackList";
 
-function App(){
-
-  const [feedback,setfeedback]=useState(FeedBackData)
-  return(
+function App() {
+  const [feedback, setfeedback] = useState(FeedBackData);
+  return (
     <>
-      <FeedBackList feedback={feedback}/>
+      <FeedBackList feedback={feedback} />
     </>
-  )
+  );
 }
-export default App
+export default App;
